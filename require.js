317c190ad82621349901cd9ddb3a63f10d2a85fe
dltnk9 <dltnk9@naver.com>
2022-04-10 18:10:44 +1000

    tetris finished, chatbox start

--- require.js
+++
@@ -1,7 +0,0 @@
-require('./var'); // 실행만 하고싶다, 변수를 가져오고싶진않다 - > require로 가져오기
-
-console.log(require);
-
-module.exports = {
-
-}
--- this.js
+++
@@ -1,8 +0,0 @@
-console.log(this); // global?
-console.log(this === module.exports) // 노드에서 전역스코프 디스는 모듈익스포츠다
-
-
-function a() {
-  console.log(this === global);
-}
-a();
